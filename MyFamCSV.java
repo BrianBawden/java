f5b4d3a6d156e4edd9e54a8cf227c0f85329d003
Brian Bawden <brianbawden001@gmail.com>
2022-04-28 14:57:40 -0400

    Added input for data fields

--- MyFamCSV.java
+++ MyFamCSV.java
@@ -25,7 +25,16 @@ public class MyFamCSV {
     static void famWriter() {
         try {
             FileWriter myWriter = new FileWriter("C:\\Users\\brian\\OneDrive\\Desktop\\java\\myFamList.txt", true);
+            myWriter.write("\n");
             myWriter.write(famInput("Name"));
+            myWriter.write(", ");
+            myWriter.write(famInput("Relation"));
+            myWriter.write(", ");
+            myWriter.write(famInput("Phone Number"));
+            myWriter.write(", ");
+            myWriter.write(famInput("State"));
+            
+
             myWriter.close();
             }catch(IOException e) {
                 System.out.println("An error occurred.");
