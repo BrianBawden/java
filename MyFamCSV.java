41337b816609b67e87bfcddacc078f0418b1ebc6
Brian Bawden <brianbawden001@gmail.com>
2022-04-28 14:43:56 -0400

    get user input for a name

--- MyFamCSV.java
+++ MyFamCSV.java
@@ -25,13 +25,19 @@ public class MyFamCSV {
     static void famWriter() {
         try {
             FileWriter myWriter = new FileWriter("C:\\Users\\brian\\OneDrive\\Desktop\\java\\myFamList.txt", true);
-            myWriter.write("\nHi Brian.");
+            myWriter.write(famInput("Name"));
             myWriter.close();
             }catch(IOException e) {
                 System.out.println("An error occurred.");
                 e.printStackTrace();
             }
         }
+
+      static String famInput(String data) {
+        Scanner myInput = new Scanner(System.in);
+        System.out.println("Enter " + data + ": ");
+        return myInput.nextLine();
+      }
     
   public static void main(String[] args) {
     famReader();
